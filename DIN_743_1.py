1fdecd1977c68e13955d8c677b797eea8c490aa4
hanslhansl <>
2024-11-25 23:29:44 +0100

    a

---
+++ DIN_743_1.py
@@ -0,0 +1,363 @@
+import math as m, sys
+from DIN_743_2 import *
+from DIN_743_3 import *
+
+
+S_min = 1.2
+print(f"S_min = {S_min}")
+print()
+
+
+
+def Festigkeit(fall, werkstoff : Werkstoff, kerbe : Kerbe, d, d_eff,
+    F_zdm, F_zda, M_bm, M_ba, M_tm, M_ta,
+    Rz, K_A, K_S, K_V, harte_randschicht, **kwargs):
+    """Im Druckbereich sind sigma_zdm und sigma_bm negativ"""
+
+    assert(fall in (1, 2))
+    
+    print(f"d = {d}")
+    print(f"d_eff = {d_eff}")
+    
+    print(f"Rz = {Rz}")
+    print(f"K_A = {K_A}")
+    print(f"K_S = {K_S}")
+    print(f"K_V = {K_V}")
+
+    def _max(m, a):
+        return m + a if a >= 0 else m - a
+    sigma_zdm = kerbe.sigma_zd(F_zdm, d)
+    sigma_zda = kerbe.sigma_zd(F_zda, d)
+    sigma_zdmax = K_S * _max(sigma_zdm, sigma_zda)
+    sigma_zda *= K_A
+    print(f"sigma_zdm = {sigma_zdm}")
+    print(f"sigma_zda = {sigma_zda}")
+    print(f"sigma_zdmax = {sigma_zdmax}")
+
+    sigma_bm = kerbe.sigma_b(M_bm, d)
+    sigma_ba = kerbe.sigma_b(M_ba, d)
+    sigma_bmax = K_S * _max(sigma_bm, sigma_ba)
+    sigma_ba *= K_A
+    print(f"sigma_bm = {sigma_bm}")
+    print(f"sigma_ba = {sigma_ba}")
+    print(f"sigma_bmax = {sigma_bmax}")
+
+    tau_tm = kerbe.tau_t(M_tm, d)
+    tau_ta = kerbe.tau_t(M_ta, d)
+    tau_tmax = K_S * _max(tau_tm, tau_ta)
+    tau_ta *= K_A
+    print(f"tau_tm = {tau_tm}")
+    print(f"tau_ta = {tau_ta}")
+    print(f"tau_tmax = {tau_tmax}")
+
+    sigma_B_d_B = werkstoff.sigma_B_d_B
+    sigma_S_d_B = werkstoff.sigma_S_d_B
+    print(f"sigma_B_d_B = {sigma_B_d_B}")
+    print(f"sigma_S_d_B = {sigma_S_d_B}")
+
+    sigma_zdW_d_B = werkstoff.sigma_zdW_d_B
+    sigma_bW_d_B = werkstoff.sigma_bW_d_B
+    tau_tW_d_B = werkstoff.tau_tW_d_B
+    print(f"sigma_zdW_d_B = {sigma_zdW_d_B}")
+    print(f"sigma_bW_d_B = {sigma_bW_d_B}")
+    print(f"tau_tW_d_B = {tau_tW_d_B}")
+
+    K_1B_d_eff = K_1(werkstoff=werkstoff, d_eff=d_eff, zugfestigkeit=True)
+    K_1S_d_eff = K_1(werkstoff=werkstoff, d_eff=d_eff, zugfestigkeit=False)
+    print(f"K_1B_d_eff = {K_1B_d_eff}")
+    print(f"K_1S_d_eff = {K_1S_d_eff}")
+
+    # 743-2 Abschnitt 7
+    sigma_B_d_eff = K_1B_d_eff * sigma_B_d_B
+    print(f"sigma_B_d_eff = {sigma_B_d_eff}")
+    # keine Ahnung
+    sigma_B_d = sigma_B_d_B * K_1B_d_eff
+    print(f"sigma_B_d = {sigma_B_d}")
+    # Abschnitt 5.2
+    sigma_S_d = sigma_S_d_B * K_1S_d_eff
+    print(f"sigma_S_d = {sigma_S_d}")
+
+    K_2_d_zd = K_2_zd(d=d)
+    K_2_d_b = K_2_b(d=d)
+    K_2_d_t = K_2_t(d=d)
+    print(f"K_2_d_zd = {K_2_d_zd}")
+    print(f"K_2_d_b = {K_2_d_b}")
+    print(f"K_2_d_t = {K_2_d_t}")
+
+    K_Fsigma = kerbe.K_Fsigma(Rz=Rz, sigma_B_d_eff=sigma_B_d_eff)
+    K_Ftau = kerbe.K_Ftau(Rz=Rz, sigma_B_d_eff=sigma_B_d_eff)
+    print(f"K_Fsigma = {K_Fsigma}")
+    print(f"K_Ftau = {K_Ftau}")
+
+    beta_sigma_zd = kerbe.beta_sigma_zd(d=d, sigma_B_d=sigma_B_d, sigma_S_d=sigma_S_d, harte_randschicht=harte_randschicht)
+    beta_sigma_b = kerbe.beta_sigma_b(d, sigma_B_d=sigma_B_d, sigma_S_d=sigma_S_d, harte_randschicht=harte_randschicht)
+    beta_tau = kerbe.beta_tau(d, sigma_B_d=sigma_B_d, sigma_S_d=sigma_S_d, harte_randschicht=harte_randschicht)
+    print(f"beta_sigma_zd = {beta_sigma_zd}")
+    print(f"beta_sigma_b = {beta_sigma_b}")
+    print(f"beta_tau = {beta_tau}")
+  
+    # Glg 8, 9
+    K_sigma_zd = (beta_sigma_zd / K_2_d_zd + 1 / K_Fsigma - 1) / K_V
+    K_sigma_b = (beta_sigma_b / K_2_d_b + 1 / K_Fsigma - 1) / K_V
+    K_tau = (beta_tau / K_2_d_t + 1 / K_Ftau - 1) / K_V
+    print(f"K_sigma_zd = {K_sigma_zd}")
+    print(f"K_sigma_b = {K_sigma_b}")
+    print(f"K_tau = {K_tau}")
+
+    # Glg 5-7
+    sigma_zdWK = sigma_zdW_d_B * K_1B_d_eff / K_sigma_zd
+    sigma_bWK = sigma_bW_d_B * K_1B_d_eff / K_sigma_b
+    tau_tWK = tau_tW_d_B * K_1B_d_eff / K_tau
+    print(f"sigma_zdWK = {sigma_zdWK}")
+    print(f"sigma_bWK = {sigma_bWK}")
+    print(f"tau_tWK = {tau_tWK}")
+
+    # Glg 20-22
+    psi_zdsigmaK = sigma_zdWK / (2 * K_1B_d_eff * sigma_B_d_B - sigma_zdWK)
+    psi_bsigmaK = sigma_bWK / (2 * K_1B_d_eff * sigma_B_d_B - sigma_bWK)
+    psi_tauK = tau_tWK / (2 * K_1B_d_eff * sigma_B_d_B - tau_tWK)
+    print(f"psi_zdsigmaK = {psi_zdsigmaK}")
+    print(f"psi_bsigmaK = {psi_bsigmaK}")
+    print(f"psi_tauK = {psi_tauK}")
+
+    # Glg 23, 24
+    sigma_mv = m.sqrt((sigma_zdm + sigma_bm)**2 + 3 * tau_tm**2) 
+    tau_mv = sigma_mv / m.sqrt(3)
+    print(f"sigma_mv = {sigma_mv}")
+    print(f"tau_mv = {tau_mv}")
+
+    # Tabelle 2
+    def gamma_F(beta_sigma):
+        if not kerbe.umdrehungskerbe or harte_randschicht:
+            return 1.
+        else:
+            if beta_sigma < 1.5:
+                return 1.
+            elif beta_sigma < 2:
+                return 1.05
+            elif beta_sigma < 3:
+                return 1.1
+            else:
+                return 1.15
+    gamma_Fzd = gamma_F(beta_sigma_zd)
+    gamma_Fb = gamma_F(beta_sigma_b)
+    gamma_Ft = 1.
+    print(f"gamma_Fzd = {gamma_Fzd}")
+    print(f"gamma_Fb = {gamma_Fb}")
+    print(f"gamma_Ft = {gamma_Ft}")
+
+    # K_2F, Tabelle 3, vernachlässigen der Hohlwelle
+    if harte_randschicht:
+        K_2Fzd = 1.0
+        K_2Fb = 1.0
+        K_2Ft = 1.0
+    else:
+        K_2Fzd = 1.0
+        K_2Fb = 1.2
+        K_2Ft = 1.2
+    print(f"K_2Fzd = {K_2Fzd}")
+    print(f"K_2Fb = {K_2Fb}")
+    print(f"K_2Ft = {K_2Ft}")
+
+    # Glg 31, 32
+    sigma_zdFK = K_1S_d_eff * K_2Fzd * gamma_Fzd * sigma_S_d_B
+    sigma_bFK = K_1S_d_eff * K_2Fb * gamma_Fb * sigma_S_d_B
+    tau_tFK = K_1S_d_eff * K_2Ft * gamma_Ft * sigma_S_d_B / m.sqrt(3)
+    print(f"sigma_zdFK = {sigma_zdFK}")
+    print(f"sigma_bFK = {sigma_bFK}")
+    print(f"tau_tFK = {tau_tFK}")
+    
+    assert not (sigma_zdm + sigma_bm < 0)
+    assert not (sigma_mv < 0)
+    if fall == 1:
+        def ADK(mv, FK, WK, psi):
+            if mv <= (FK - WK) / (1 - psi):
+                return WK - psi * mv
+            else:
+                return FK - mv
+        sigma_zdADK = ADK(sigma_mv, sigma_zdFK, sigma_zdWK, psi_zdsigmaK)
+        sigma_bADK = ADK(sigma_mv, sigma_bFK, sigma_bWK, psi_bsigmaK)
+        tau_tADK = ADK(tau_mv, tau_tFK, tau_tWK, psi_tauK)
+        print(f"sigma_zdADK = {sigma_zdADK}")
+        print(f"sigma_bADK = {sigma_bADK}")
+        print(f"tau_tADK = {tau_tADK}")
+    else:
+        def ADK(mv, a, FK, WK, psi):
+            if mv / a <= (FK - WK) / (WK - FK * psi):
+                return WK / (1 + psi * mv / a)
+            else:
+                return FK / (1 + mv / a)
+            
+        if sigma_zda != 0:
+            sigma_zdADK = ADK(sigma_mv, sigma_zda, sigma_zdFK, sigma_zdWK, psi_zdsigmaK)
+            print(f"sigma_zdADK = {sigma_zdADK}")
+        if sigma_ba != 0:
+            sigma_bADK = ADK(sigma_mv, sigma_ba, sigma_bFK, sigma_bWK, psi_bsigmaK)
+            print(f"sigma_bADK = {sigma_bADK}")
+        if tau_ta != 0:
+            tau_tADK = ADK(tau_mv, tau_ta, tau_tFK, tau_tWK, psi_tauK)
+            print(f"tau_tADK = {tau_tADK}")
+
+    temp = 0
+    if sigma_zda != 0:
+        temp += sigma_zda / sigma_zdADK
+    if sigma_ba != 0:
+        temp += sigma_ba / sigma_bADK
+    temp = temp**2
+    if tau_ta != 0:
+        temp += (tau_ta / tau_tADK)**2
+    if temp == 0:
+        print("\033[93mSicherheit gegen Dauerbruch nicht berechnet\033[0m")
+    else:
+        S_Dauer = 1 / m.sqrt(temp)
+        print(f"S_Dauer = {S_Dauer}")
+        if not S_Dauer >= S_min:
+            print("\033[91mSicherheit gegen Dauerbruch ist nicht erfuellt\033[0m")
+
+    temp = 0
+    if sigma_zdmax:
+        temp += sigma_zdmax / sigma_zdFK
+    if sigma_bmax != 0:
+        temp += sigma_bmax / sigma_bFK
+    temp = temp**2
+    if tau_tmax != 0:
+        temp += (tau_tmax / tau_tFK)**2
+    if temp == 0:
+        print("\033[93mSicherheit gegen bleibende Verformungen nicht berechnet\033[0m")
+    else:
+        S_Verform = 1 / m.sqrt(temp)
+        print(f"S_Verform = {S_Verform}")
+        if not S_Verform >= S_min:
+            print("\033[91mSicherheit gegen bleibende Verformungen ist nicht erfuellt\033[0m")
+
+    print()
+    return
+
+# 1
+Festigkeit(fall = 1,
+    werkstoff = Werkstoff._50CrMo4,
+    kerbe = Absatz(4, 8),
+    d = 84,
+    d_eff = 100,
+    F_zdm = 443341,
+    F_zda = 0,
+    M_bm = 23280,
+    M_ba = 2910,
+    M_tm = 13970,
+    M_ta = 0,
+    Rz = 10,
+    K_A = 1.,
+    K_S = 1.,
+    K_V = 1,
+    harte_randschicht = False)
+# 2
+Festigkeit(fall = 2,
+    werkstoff = Werkstoff.S235,
+    kerbe = Spitzkerbe(10),
+    d = 80,
+    d_eff = 80,
+    F_zdm = 40000,
+    F_zda = 0,
+    M_bm = 10666.667,
+    M_ba = 0,
+    M_tm = 0,
+    M_ta = 0,
+    Rz = 25,
+    K_A = 1.,
+    K_S = 1.5,
+    K_V = 1,
+    harte_randschicht = False)
+# 3
+Festigkeit(fall = 2,
+    werkstoff = Werkstoff.C50,
+    kerbe = Passfeder(1),
+    d = 60,
+    d_eff = 80,
+    F_zdm = 0,
+    F_zda = 0,
+    M_bm = 0,
+    M_ba = 1402.294,
+    M_tm = 1336.902,
+    M_ta = 0,
+    Rz = 25,
+    K_A = 1.,
+    K_S = 1.8,
+    K_V = 1,
+    harte_randschicht = True)
+# 4
+Festigkeit(fall = 2,
+    werkstoff = Werkstoff.E335,
+    kerbe = Querbohrung(2),
+    d = 50,
+    d_eff = 80,
+    F_zdm = 0,
+    F_zda = 0,
+    M_bm = 0,
+    M_ba = 1088.802,
+    M_tm = 596.831,
+    M_ta = 0,
+    Rz = 25,
+    K_A = 1.,
+    K_S = 2.,
+    K_V = 1,
+    harte_randschicht = False)
+
+
+
+print("Passfeder Ritzel")
+Festigkeit(fall = 2,
+    werkstoff = Werkstoff._34CrNiMo6,
+    kerbe = Passfeder(1),
+    d = 44,
+    d_eff = 44,
+    F_zdm = 0,
+    F_zda = 0,
+    M_bm = 0,
+    M_ba = 513.681,
+    M_tm = 535.93,
+    M_ta = 0,
+    Rz = 16,
+    K_A = 1.75,
+    K_S = 2.5,
+    K_V = 1,
+    harte_randschicht = False)
+
+"""    F_zda = 0,
+    F_zdm = 0,
+    M_ba = 950.309,
+    M_bm = 0,
+    M_bmax = 1284.202,
+    M_ta = 0,
+    M_tm = 991.415,
+    M_tmax = 1339.75,
+    R_Z = 16,
+    K_A = 1.75,
+    K_S = 2.5)"""
+
+print("Passfeder Rad")
+Festigkeit(fall = 2,
+    werkstoff = Werkstoff.C60,
+    kerbe = Passfeder(1),
+    d = 94,
+    d_eff = 94,
+    F_zdm = 0, 
+    F_zda = 0, 
+    M_bm = 0,
+    M_ba = 513.681, 
+    M_tm = 2933.511,
+    M_ta = 0, 
+    Rz = 16,
+    K_A = 1.75,
+    K_S = 2.5,
+    K_V = 1,
+    harte_randschicht = False)
+
+"""F_zda = 0, 
+    F_zdm = 0, 
+    F_zdmax = 0,
+    M_ba = 950.309, 
+    M_bm = 0,
+    M_bmax = 1284.202,
+    M_ta = 0, 
+    M_tm = 5390.812,
+    M_tmax = 7284.88,"""
---
+++ DIN_743_2.py
@@ -0,0 +1,431 @@
+import math as m
+from abc import abstractmethod
+from DIN_743_3 import *
+
+
+class Kerbe:
+    def sigma_zd(self, F_zd, d):
+        return F_zd / (m.pi / 4 * d**2)
+    def sigma_b(self, M_b, d):
+        return M_b / (m.pi / 32 * d**3) * 1000
+    def tau_t(self, M_t, d):
+        return M_t / (m.pi / 16 * d**3) * 1000
+
+    def D(self, d):
+        return d + 2 * self.t
+    # @abstractmethod
+    # def beta_sigma_zd(self, d, **kwargs):
+    #     pass
+    # @abstractmethod
+    # def beta_sigma_b(self, d, **kwargs):
+    #     pass
+    # @abstractmethod
+    # def beta_tau(self, d, **kwargs):
+    #     pass
+    
+    # @abstractmethod
+    # def K_Fsigma(self, Rz, sigma_B_d_eff):
+    #     pass
+    # @abstractmethod
+    # def K_Ftau(self, Rz, sigma_B_d_eff):
+    #     pass
+    pass
+
+
+class K_F_nach_Welle_Nabe:
+    """Tabelle 1"""
+    def K_Fsigma(self, Rz, sigma_B_d_eff):
+        return 1
+    def K_Ftau(self, Rz, sigma_B_d_eff):
+        return 1
+class K_F_nach_Formel:
+    """Glg 18 & 19"""
+    def _K_Fsigma(self, Rz, sigma_B_d_eff):
+        return 1 - 0.22 * m.log(Rz, 10) * (m.log(sigma_B_d_eff / 20, 10) - 1)
+    def _K_Ftau(self, Rz, sigma_B_d_eff):
+        return 0.575 * self._K_Fsigma(Rz, sigma_B_d_eff) + 0.425
+
+    def K_Fsigma(self, Rz, sigma_B_d_eff):
+        if hasattr(self, "Rz_B"):
+           return self._K_Fsigma(Rz, sigma_B_d_eff) / self._K_Fsigma(self.Rz_B, sigma_B_d_eff)
+        return self._K_Fsigma(Rz, sigma_B_d_eff)
+    def K_Ftau(self, Rz, sigma_B_d_eff):
+        if hasattr(self, "Rz_B"):
+           return self._K_Ftau(Rz, sigma_B_d_eff) / self._K_Ftau(self.Rz_B, sigma_B_d_eff)
+        return self._K_Ftau(Rz, sigma_B_d_eff)
+
+
+class ExperimentelleKerbwirkungszahlen(Kerbe):
+    def beta_sigma_zd(self, d, **kwargs):
+        beta_d_BK = self.beta_sigma_zd_d_BK(**kwargs)
+        K_3_d = self.K_3(d, beta_d_BK)
+        K_3_d_BK = self.K_3(self.d_BK, beta_d_BK)
+        print(f"beta_sigma_zd_d_BK = {beta_d_BK}")
+        print(f"K_3_zd_d_BK = {K_3_d_BK}")
+        print(f"K_3_zd_d = {K_3_d}")
+        return beta_d_BK * K_3_d_BK / K_3_d
+    def beta_sigma_b(self, d, **kwargs):
+        beta_d_BK = self.beta_sigma_b_d_BK(**kwargs)
+        K_3_d = self.K_3(d, beta_d_BK) 
+        K_3_d_BK = self.K_3(self.d_BK, beta_d_BK)
+        print(f"beta_sigma_b_d_BK = {beta_d_BK}")
+        print(f"K_3_b_d_BK = {K_3_d_BK}")
+        print(f"K_3_b_d = {K_3_d}")
+        return beta_d_BK * K_3_d_BK / K_3_d
+    def beta_tau(self, d, **kwargs):
+        beta_d_BK = self.beta_tau_d_BK(**kwargs)
+        K_3_d = self.K_3(d, beta_d_BK)
+        K_3_d_BK = self.K_3(self.d_BK, beta_d_BK)
+        print(f"beta_tau_d_BK = {beta_d_BK}")
+        print(f"K_3_t_d_BK = {K_3_d_BK}")
+        print(f"K_3_t_d = {K_3_d}")
+        return beta_d_BK * K_3_d_BK / K_3_d
+
+    def K_3(self, d, alpha):
+        if 7.5 <= d < 150:
+            return 1 - 0.2 * m.log(alpha, 10) * m.log(d / 7.5, 10) / m.log(20, 10)
+        elif d >= 150:
+            return 1 - 0.2 * m.log(alpha)
+        raise NotImplementedError
+
+class Passfeder(ExperimentelleKerbwirkungszahlen, K_F_nach_Welle_Nabe):
+    """Tabelle 1"""
+    umdrehungskerbe = False
+
+    def __init__(self, i : int):
+        self.d_BK = 40
+        self.i = i
+        assert i in (1, 2)
+        
+    def beta_sigma_zd_d_BK(self, sigma_B_d: float, **kwargs):
+        return (3 * (sigma_B_d / 1000)**0.38) * (1.15 if self.i == 2 else 1)
+    def beta_sigma_b_d_BK(self, sigma_B_d: float, **kwargs):
+        return self.beta_sigma_zd_d_BK(sigma_B_d)
+    def beta_tau_d_BK(self, sigma_B_d: float, **kwargs):
+        return (0.56 * 3 * (sigma_B_d / 1000)**0.38 + 0.1) * (1.15 if self.i == 2 else 1)
+
+class Presssitz(ExperimentelleKerbwirkungszahlen, K_F_nach_Welle_Nabe):
+    """Tabelle 1"""
+    umdrehungskerbe = True
+
+    def __init__(self):
+        self.d_BK = 40
+
+    def beta_sigma_zd_d_BK(self, sigma_B_d: float, **kwargs):
+        return 2.7 * (sigma_B_d / 1000)**0.43
+    def beta_sigma_b_d_BK(self, sigma_B_d: float, **kwargs):
+        return self.beta_sigma_zd_d_BK(sigma_B_d)
+    def beta_tau_d_BK(self, sigma_B_d: float, **kwargs):
+        return 0.65 * self.beta_sigma_d_BK(sigma_B_d)
+
+    def sigma_zd(self, F_zd, d):
+        return F_zd / (m.pi / 4 * d**2)
+    def sigma_b(self, M_b, d):
+        return M_b / (m.pi / 32 * d**3)
+    def sigma_t(self, M_t, d):
+        return M_t / (m.pi / 16 * d**3)
+
+class Spitzkerbe(ExperimentelleKerbwirkungszahlen, K_F_nach_Formel):
+    """Abschnitt 4.2.3"""
+    umdrehungskerbe = True
+    r = 0.1
+    d_BK = 15
+    Rz_B = 20
+
+    def __init__(self, t):
+        self.t = t
+
+    def beta_sigma_zd_d_BK(self, sigma_B_d: float, **kwargs):
+        return 0.109 * sigma_B_d / 100 + 1.074
+    def beta_sigma_b_d_BK(self, sigma_B_d: float, **kwargs):
+        return 0.0923 * sigma_B_d / 100 + 0.985
+    def beta_tau_d_BK(self, sigma_B_d: float, **kwargs):
+        return 0.8 * self.beta_sigma_b_d_BK(sigma_B_d)
+
+    def sigma_zd(self, F_zd, d):
+        assert 0.05 <= self.t/d <= 0.20
+        return super().sigma_zd(F_zd, d)
+    def sigma_b(self, M_b, d):
+        assert 0.05 <= self.t/d <= 0.20
+        return super().sigma_b(M_b, d)
+    def sigma_t(self, M_t, d):
+        assert 0.05 <= self.t/d <= 0.20
+        return super().sigma_t(M_t, d)
+
+class UmlaufendeRechtecknut(ExperimentelleKerbwirkungszahlen, K_F_nach_Formel):
+    """Abschnitt 4.2.4"""
+    umdrehungskerbe = True
+    d_BK = 30
+    Rz_B = 20
+
+    def __init__(self, t, r):
+        raise NotImplementedError
+        self.t = t
+        self.r = r
+
+    def rho_s(self, sigma_S_d):
+        return 10 ** -(0.514 + 0.00152 - sigma_S_d)
+
+    def beta_sigma_zd_d_BK(self, sigma_S_d: float, **kwargs):
+        r_f = self.r + 2.9 * self.rho_s(sigma_S_d)
+        return min(0.9 * (1.27 + 1.17 * m.sqrt(self.t / r_f)), 4)
+    def beta_sigma_b_d_BK(self, sigma_S_d: float, **kwargs):
+        r_f = self.r + 2.9 * self.rho_s(sigma_S_d)
+        return min(0.9 * (1.14 + 1.08 * m.sqrt(self.t / r_f)), 4)
+    def beta_tau_d_BK(self, sigma_S_d: float, **kwargs):
+        r_f = self.r + self.rho_s(sigma_S_d)
+        return min(1.48 + 0.45 * m.sqrt(self.t / r_f), 2.5)
+
+
+class BekannteFormzahl(Kerbe, K_F_nach_Formel):
+    """4.3.1"""
+    def beta_sigma_zd(self, d, **kwargs):
+        alpha = self.alpha_sigma_zd(d=d)
+        n = self.n_zd(d=d, **kwargs)
+        print(f"alpha_sigma_zd = {alpha}")
+        print(f"n_zd = {n}")
+        return alpha / n
+    def beta_sigma_b(self, d, **kwargs):
+        alpha = self.alpha_sigma_b(d=d)
+        n = self.n_b(d=d, **kwargs)
+        print(f"alpha_sigma_b = {alpha}")
+        print(f"n_b = {n}")
+        return alpha / n
+    def beta_tau(self, d, **kwargs):
+        alpha = self.alpha_tau(d=d)
+        n = self.n_t(d=d, **kwargs)
+        print(f"alpha_tau = {alpha}")
+        print(f"n_t = {n}")
+        return alpha / n
+    
+    def n_zd(self, sigma_S_d, harte_randschicht, **kwargs):
+        G_s = self.G_zd_s(**kwargs)
+        print(f"G_zd_s = {G_s}")
+        if harte_randschicht:
+            return 1 + m.sqrt(G_s) * 10 ** -0.7
+        else:
+            return 1 + m.sqrt(G_s) * 10 ** -(0.33 + sigma_S_d / 712)
+    def n_b(self, sigma_S_d, harte_randschicht, **kwargs):
+        G_s = self.G_b_s(**kwargs)
+        print(f"G_b_s = {G_s}")
+        if harte_randschicht:
+            return 1 + m.sqrt(G_s) * 10 ** -0.7
+        else:
+            return 1 + m.sqrt(G_s) * 10 ** -(0.33 + sigma_S_d / 712)
+    def n_t(self, sigma_S_d, harte_randschicht, **kwargs):
+        G_s = self.G_t_s(**kwargs)
+        print(f"G_t_s = {G_s}")
+        if harte_randschicht:
+            return 1 + m.sqrt(G_s) * 10 ** -0.7
+        else:
+            return 1 + m.sqrt(G_s) * 10 ** -(0.33 + sigma_S_d / 712)
+
+class AbsatzUndRundnut(BekannteFormzahl):
+    umdrehungskerbe = True
+
+    def _alpha(self, d, A, B, C, z):
+        D = self.D(d)
+        alpha = 1 + 1 / m.sqrt(A * self.r / self.t + 2 * B * self.r / d * (1 + 2 * self.r / d)**2 + C * (self.r / self.t)**z * d / D)
+        assert self.r / self.t >= 0.03
+        assert d / D <= 0.98
+        assert alpha <= 6
+        return alpha
+    def alpha_sigma_zd(self, d):
+        return self._alpha(d, self.A_zd(), self.B_zd(), self.C_zd(), self.z_zd())
+    def alpha_sigma_b(self, d):
+        return self._alpha(d, self.A_b(), self.B_b(), self.C_b(), self.z_b())
+    def alpha_tau(self, d):
+        return self._alpha(d, self.A_t(), self.B_t(), self.C_t(), self.z_t())
+
+class Rundnut(AbsatzUndRundnut):
+    """Tabelle 2"""
+    def __init__(self, r, t):
+        self.r = r
+        self.t = t
+        
+    def phi(self, d):
+        if d / self.D(d) > 0.67 and self.r > 0:
+            return 1 / (4 * m.sqrt(self.t / self.r) + 2)
+        return 0
+
+    def G_zd_s(self, **kwargs):
+        return 2 * (1 + self.phi(**kwargs)) / self.r
+    def G_b_s(self, **kwargs):
+        return 2 * (1 + self.phi(**kwargs)) / self.r
+    def G_t_s(self, **kwargs):
+        return 1 / self.r
+    
+    def A_zd(self):
+        return 0.22
+    def A_b(self):
+        return 0.2
+    def A_t(self):
+        return 0.7
+
+    def B_zd(self):
+        return 1.37
+    def B_b(self):
+        return 2.75
+    def B_t(self):
+        return 10.3
+    
+    def C_zd(self):
+        return 0
+    def C_b(self):
+        return 0
+    def C_t(self):
+        return 0
+
+    def z_zd(self):
+        return 0
+    def z_b(self):
+        return 0
+    def z_t(self):
+        return 0
+
+class Absatz(AbsatzUndRundnut):
+    """Tabelle 2"""
+    def __init__(self, r, t):
+        self.r = r
+        self.t = t
+        
+    def phi(self, d, **kwargs):
+        if d / self.D(d) > 0.67 and self.r > 0:
+            return 1 / (4 * m.sqrt(self.t / self.r) + 2)
+        return 0
+
+    def G_zd_s(self, **kwargs):
+        return 2.3 * (1 + self.phi(**kwargs)) / self.r
+    def G_b_s(self, **kwargs):
+        return 2.3 * (1 + self.phi(**kwargs)) / self.r
+    def G_t_s(self, **kwargs):
+        return 1.15 / self.r
+
+    def A_zd(self):
+        return 0.62
+    def A_b(self):
+        return 0.62
+    def A_t(self):
+        return 3.4
+
+    def B_zd(self):
+        return 3.5
+    def B_b(self):
+        return 5.8
+    def B_t(self):
+        return 19
+    
+    def C_zd(self):
+        return 0
+    def C_b(self):
+        return 0.2
+    def C_t(self):
+        return 1
+
+    def z_zd(self):
+        return 0
+    def z_b(self):
+        return 3
+    def z_t(self):
+        return 2
+
+class Freistrich(Absatz):
+    def __init__(self, r, t, D):
+        self.r = r
+        self.t = t
+        self.D = D
+
+    def D(self, d):
+        return self.D
+
+class Querbohrung(BekannteFormzahl):
+    """Abschnitt 5.2.3 """
+    umdrehungskerbe = False
+
+    def __init__(self, r):
+        self.r = r
+
+    def alpha_sigma_zd(self, d):
+        return 3 - (2 * self.r / d)
+    def alpha_sigma_b(self, d):
+        return 3 + 1.4 * (2 * self.r / d) - 2.8 * m.sqrt(2 * self.r / d)
+    def alpha_tau(self, d):
+        return 2.023 - 1.125 * m.sqrt(2 * self.r / d)
+
+    def G_zd_s(self, **kwargs):
+        return 2.3 / self.r
+    def G_b_s(self, d, **kwargs):
+        return 2.3 / self.r + 2 / d
+    def G_t_s(self, d, **kwargs):
+        return 1.15 / self.r + 2 / d
+
+    def sigma_zd(self, F_zd, d):
+        return F_zd / (m.pi * d**2 / 4 - 2 * self.r * d)
+    def sigma_b(self, M_b, d):
+        return M_b / (m.pi * d**3 / 32 - self.r * d**2 / 3) * 1000
+    def tau_t(self, M_t, d):
+        return M_t / (m.pi * d**3 / 16 - self.r * d**2 / 3) * 1000
+
+
+def K_1(werkstoff: Werkstoff, d_eff, zugfestigkeit : bool):
+    """
+    Glg 10-14
+    zugfestigkeit: True für sigma_B, False für sigma_S
+    """
+    streckgrenze = not zugfestigkeit
+    if werkstoff.art == Werkstoff.Art.Nitrierstahl or zugfestigkeit and werkstoff.art == Werkstoff.Art.Baustahl:
+        if d_eff <= 100:
+            return 1.
+        elif d_eff < 300:
+            return 1 - 0.23 * m.log(d_eff / 100, 10)
+        elif d_eff <= 500:
+            return 0.89
+    elif streckgrenze and werkstoff.art == Werkstoff.Art.Baustahl:
+        if d_eff <= 32:
+            return 1.
+        elif d_eff < 300:
+            d_B = 16
+            return 1 - 0.26 * m.log(d_eff / 2 / d_B, 10)
+        elif d_eff <= 500:
+            return 0.75
+    elif werkstoff.art == Werkstoff.Art.CrNiMoEinsatzstahl or zugfestigkeit and Werkstoff.Art.vergüteterStahl:
+        d_B = 16
+        if d_eff <= 16:
+            return 1.
+        elif d_eff < 300:
+            return 1 - 0.26 * m.log(d_eff / d_B, 10)
+        elif d_eff <= 500:
+            return 0.67
+    elif werkstoff.art == Werkstoff.Art.andererEinsatzstahl:
+        if d_eff <= 16:
+            return 1.
+        elif d_eff < 150:
+            d_B = 16
+            return 1 - 0.41 * m.log(d_eff / d_B, 10)
+        elif d_eff <= 500:
+            return 0.6
+    elif werkstoff.art == Werkstoff.Art.vergüteterStahl:
+        d_B = 16
+        if d_eff <= 16:
+            return 1.
+        elif d_eff < 300:
+            return 1 - 0.34 * m.log(d_eff / d_B, 10)
+        elif d_eff <= 500:
+            return 0.57
+
+    raise NotImplementedError
+
+def K_2_zd(d):
+    """Glg 15"""
+    return 1
+def K_2_b(d):
+    """Glg 16"""
+    if 7.5 <= d < 150:
+        return 1 - 0.2 * m.log(d / 7.5, 10) / m.log(20, 10)
+    elif d >= 150:
+        return 0.8
+    raise NotImplementedError
+def K_2_t(d):
+    """Glg 16"""
+    return K_2_b(d)
+
